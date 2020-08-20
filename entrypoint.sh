31d55e108f36bcc06ce2cc2ee0d118418f4c615e
Sunny Kim <sungunkim367@gmail.com>
2020-08-20 17:39:09 +0900

    Remove command trace and clean up echo statements

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -ex
+set -e
 
 # source = $1
 # strict = $2
@@ -13,53 +13,53 @@ set -ex
 echo "===== Running Python Linter - '$2' ====="
 echo "Django project? '$7'"
 
-echo "===== Running: MYPY ====="
+echo "Running mypy"
 mypy "$1" --show-error-codes --show-error-context --pretty ${3}
 
-echo "===== Running: BLACK ====="
+echo "Running black"
 if [ "$7" ]; then
   if [ "$2" = "high" ]; then
     black "$1" ${5} --exclude "migrations" --check
-    echo "===== Running: ISORT ====="
+    echo "Running isort"
     isort "$1"/**/*.py -m 3 -tc ${6} -c
-    echo "===== Running: PYLINT ====="
+    echo "Running pylint"
     pylint "$1" --load-plugins pylint_django ${4}
 
   elif [ "$2" = "medium" ]; then
     black "$1" ${5} --exclude "migrations" --check
-    echo "===== Running: ISORT ====="
+    echo "Running isort"
     isort "$1"/**/*.py -m 3 -tc ${6} -c
-    echo "===== Running: PYLINT ====="
+    echo "Running pylint"
     pylint "$1" --load-plugins pylint_django --fail-under=8 ${4}
 
   else
     black "$1" ${5} --exclude "migrations" --check
-    echo "===== Running: ISORT ====="
+    echo "Running isort"
     isort "$1"/**/*.py -m 3 -tc ${6} --diff
-    echo "===== Running: PYLINT ====="
+    echo "Running pylint"
     pylint "$1" --load-plugins pylint_django --exit-zero ${4}
   fi
 
 else
   if [ "$2" = "high" ]; then
     black "$1" ${5} --check
-    echo "===== Running: ISORT ====="
+    echo "Running isort"
     isort "$1"/**/*.py -m 3 -tc ${6} -c
-    echo "===== Running: PYLINT ====="
+    echo "Running pylint"
     pylint "$1" ${4}
 
   elif [ "$2" = "medium" ]; then
     black "$1" ${5} --check
-    echo "===== Running: ISORT ====="
+    echo "Running isort"
     isort "$1"/**/*.py -m 3 -tc ${6} -c
-    echo "===== Running: PYLINT ====="
+    echo "Running pylint"
     pylint "$1" --fail-under=8 ${4}
 
   else
     black "$1" ${5} --check
-    echo "===== Running: ISORT ====="
+    echo "Running isort"
     isort "$1"/**/*.py -m 3 -tc ${6} --diff
-    echo "===== Running: PYLINT ====="
+    echo "Running pylint"
     pylint "$1" --exit-zero ${4}
   fi
 fi
